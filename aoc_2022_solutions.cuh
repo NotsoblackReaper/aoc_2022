8201b0108f2add278968d444eada67b99e7ce1b0
Jonas <jonasd1305@gmail.com>
2022-12-04 11:23:37 +0100

    complete restart with cuda

---
+++ aoc_2022_solutions.cuh
@@ -0,0 +1,10 @@
+#ifndef AOC_SOLUTIONS
+#define AOC_SOLUTIONS
+
+namespace aoc {
+
+	__global__
+
+}
+
+#endif // !AOC_SOLUTIONS
---
+++ cuda_types.cuh
@@ -0,0 +1,10 @@
+#ifndef CUDA_TYPES
+#define CUDA_TYPES
+
+namespace aoc {
+	namespace types {
+
+	}
+}
+
+#endif // !CUDA_TYPES
