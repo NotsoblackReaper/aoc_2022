0cf5ca7208ca1507a28c62084e582991a0551dad
Jonas <jonasd1305@gmail.com>
2022-12-06 13:31:43 +0100

    day 6 optimized

--- day6.cpp
+++ day6.cpp
@@ -1,47 +1,27 @@
 #include "AOC_Solver.h"
-#include <numeric>
-#include <algorithm>
-#include <iostream>
-#include<execution>
-#include<map>
-#include<omp.h>
-uint64_t aoc::day6::part_1(const std::vector < std::string >& data) {
-	volatile uint64_t ret = 0;
-	
-	const char* s = data[0].c_str();
 
-#pragma omp parallel for shared(ret)
-	for (int i = 3; i < data[0].length(); ++i) {
-		if (ret != 0)continue;
-		std::map<char, int>chars{};
+uint64_t solve(const std::string& data, size_t n) {
+	for (size_t i = n; i < data.length();++i) {
+		uint32_t chars = 0;
+		bool dupe = false;
 
-		chars.emplace(s[i - 3], 1);
-		chars.emplace(s[i - 2], 1);
-		chars.emplace(s[i - 1], 1);
-		chars.emplace(s[i], 1);
-
-		if(chars.size()==4)
-			ret = i + 1;
+		for (int j = n - 1; j > -1; --j) {
+			if (chars & 0b1 << (data[i - j] - 'a')) {
+				dupe = true;
+				break;
+			}
+			chars = chars | 0b1 << (data[i - j] - 'a');
+		}
+		if (!dupe)
+			return i + 1;
 	}
+	return 0;
+}
 
-	return ret;
+uint64_t aoc::day6::part_1(const std::vector < std::string >& data) {
+	return solve(data[0], 4);
 }
 
 uint64_t aoc::day6::part_2(const std::vector < std::string >& data) {
-	volatile uint64_t ret = 0;
-
-	const char* s = data[0].c_str();
-
-	#pragma omp parallel for shared(ret)
-	for (int i = 3; i < data[0].length(); ++i) {
-		if (ret != 0)continue;
-		std::map<char, int>chars{};
-		for(int j=13;j>-1;--j)
-			chars.emplace(s[i - j], 1);
-
-		if (chars.size() == 14)
-			ret= i + 1;
-	}
-
-	return ret;
+	return solve(data[0], 14);
 }
